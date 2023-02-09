569f8749082b4787f12d5c0e8be563b7b1d77d2a
Olivier Vastel <vastel.olivier@gmail.com>
2023-02-09 14:25:57 +0100

    ex5 Finito pipo

---
+++ ex05/ft_print_comb.js
@@ -0,0 +1,16 @@
+
+function combinaison () {
+    let i=0;
+    let j=0;
+    let k=0;
+    for (i;i<10;i++){
+        for(j=i+1;j<10;j++){
+            for(k=j+1;k<10;k++){
+                console.log(i,j,k)
+            }
+                
+        }
+    }
+}
+
+combinaison();
